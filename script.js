1769215ce188000b553fab95b4844fa4d861c4b4
Christina Hantakas <christinahantakas@Christinas-MacBook-Air.local>
2024-08-01 22:36:39 -0700

    edit paragraph on about me page, add html for resume page, comment out code that wasn't working in css, javascript, and resume page, add paragraph to README

---
+++ script.js
@@ -0,0 +1,7 @@
+//cument.addEventListener('DOMContentLoaded', function() {
+//  const resumeButton = document.getElementById('resume-button');
+//  resumeButton.addEventListener('click', function() {
+//    window.open('https://docs.google.com/document/d/1pZClT-5j7pihGaWOtZaiUKz-zKrtqP_S/edit?usp=sharing&ouid=103556666346457540242&rtpof=true&sd=true', '_blank');
+//  });
+//});
+  
